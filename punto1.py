01bb656a0e99242245b7908ac9fe3f9c4472b8d8
Samuel Tasco <152022301+SamuelAlvarezT@users.noreply.github.com>
2024-03-06 13:59:33 -0500

    elborado hasta el momente
    elborado hasta el momente

--- punto1.py
+++ punto1.py
@@ -10,16 +10,19 @@ while inicio:
     meun = print("1.pesos a dolares \n2. pesos a euros \n3. euros a  pesos \n4. pesos a yenes \n5. salir")
     opcion = int(input("elija una opcion :"))
     if opcion == 1:
-        pass
+        hola = ( int(input("Ingrese la cantidad :")) * 3944)
+        print("equivalen a "(hola))
+    
     elif opcion == 2:
         
-        hola = ( int(input("Ingrese la cantidad")) * 3944)
-        print(hola)
+        hola = ( int(input("Ingrese la cantidad :")) * 4279)
+        print("equivalen a "(hola))
     elif opcion == 3:
-        hola = ( int(input("Ingrese la cantidad")) * 2994)
-        print(hola)
+        hola = ( int(input("Ingrese la cantidad :")) // 4279)
+        print("equivalen a "(hola))
     elif opcion == 4:
-        hola = (int(input("Ingrese la cantidad")) * 4270)
-        print(hola)
+        hola = (int(input("Ingrese la cantidad :")) * 26.30)
+        print("equivalen a "(hola))
     elif opcion == 5:
+        print("fin del programa.")
         break
--- punto3.py
+++ punto3.py
@@ -1,12 +1,39 @@
-print("""
-                    ++++++++++++++++++++++++
-                    +  productos vivieres  +
-                    ++++++++++++++++++++++++
-      """)
-menu3 = print("1. registrar nuevo producto \n2. salir")
-opcion = input("elija una opcion :")
-
 start = True
 while start:
       listel = [1, 2]
-      pass
+      print("""
+                        ++++++++++++++++++++++
+                        + registrar producto +
+                        ++++++++++++++++++++++
+        """)
+meun = print("1. registrar nuevo producto \n2. salir")
+opcion = int(input("elija una opcion :"))
+if opcion == 1:
+      nombre = input('Ingrese el nombre: ')
+      email = input('Ingrese el correo electrónico: ')
+      id
+      nombres
+      apellidos
+      ubicacion
+      email
+      edad
+      ocupacion
+    
+
+    telefonos = {
+        'movil': '',
+        'casa' : '',
+        'personal': '',
+        'oficina':''
+
+
+      with open('productos.json', 'w') as archivo:
+        json.dump(productos, archivo, indent=4)
+        
+ cargar_productos():
+    try:
+        with open('inventario.json', 'r') as archivo:
+            productos = json.load(archivo)
+    except FileNotFoundError:
+        productos = []
+    return productos
